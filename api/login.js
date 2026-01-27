200ab1ff90e030f32e071bb1323111fc63a4aeb6
unknown <farhandian2124@gmail.com>
2026-01-27 22:17:19 +0800

    update

--- api/login.js
+++ api/login.js
@@ -1,25 +1,13 @@
-import jwt from "jsonwebtoken";
-
-const USERNAME = "workerdaguku";
-const PASSWORD = "worker123";
-const SECRET = "daguku_secret_key_2026";
-
 export default function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
+    return res.status(405).json({ error: "Method not allowed" });
   }
 
   const { username, password } = req.body;
 
-  if (username === USERNAME && password === PASSWORD) {
-    const token = jwt.sign(
-      { user: username },
-      SECRET,
-      { expiresIn: "1d" }
-    );
-
-    return res.status(200).json({ token });
+  if (username === "worker" && password === "12345") {
+    return res.status(200).json({ token: "daguku-token" });
   }
 
-  return res.status(401).json({ message: "Login gagal" });
+  return res.status(401).json({ error: "Login gagal" });
 }
